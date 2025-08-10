2cfa4c43036ad2f812d26c06eb2322fa6fb1f2d3
João Victor de Souza Silva <joao1452019@gmail.com>
2025-08-10 17:19:14 -0300

    JAVASCRIPT

--- Scripts/function.js
+++ Scripts/function.js
@@ -1,5 +1,12 @@
-function moverCaixa() {
+function sumir() {
+    no.style.display = 'none';
+    yes.style.height = '150px';
+    yes.style.width = '250px';
+    window.alert("Sinto muito mas essa opção não está disponível");
+}
 
+function aceitar(){
+    window.alert("PARABÉNS VOCÊ ACABA DE SER PROMOVIDA A MINHA NAMORADA !!!!!");
 }
 function displayPedido(){
     declaration.style.display = 'none';
